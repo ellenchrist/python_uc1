e2f474f3623b7ab27930c7b8a3c3fd12dec94d29
ellenchrist <ellenchrist85@hotmail.com>
2025-03-07 20:42:27 -0300

    Aula 03 - Atividades

---
+++ aula03_tarefa02.py
@@ -0,0 +1,29 @@
+a=input("Digite o 1º numero : ")
+a=float(a)
+b=float(input("Digite o 2º numero : "))
+
+soma=a+b
+sub=a-b
+multiplicacao=a*b
+div_real=a/b
+div_inteira=a//b
+modulo=a%b
+exponencial=a**b
+
+#print(soma)
+
+##
+# Metodo pt_BR
+##
+print (f"O resultado da soma é {soma}")
+#print(sub)
+
+print (f"O resultado da sub é {sub}")
+
+print (f"O resultado da multiplicacao é {multiplicacao}") 
+print (f"O resultado da div_real é {div_real}") 
+print (f"O resultado da div_inteira é {div_inteira}")
+print (f"O resultado do modulo é {modulo}")  
+print (f"O resultado da exponencial é {exponencial}") 
+
+       
