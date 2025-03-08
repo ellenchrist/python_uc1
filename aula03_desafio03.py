fb840223def973828adffa6b8015eab87d20e4a3
ellenchrist <ellenchrist85@hotmail.com>
2025-03-07 21:06:56 -0300

    Aula03_Desafio03.05.py

---
+++ aula03_desafio03.py
@@ -0,0 +1,13 @@
+peso=input("Digite seu peso : ")
+peso=float(peso)
+altura=input("Digite sua altura : ")
+altura=float(altura)
+IMC=(peso)/(altura**2)
+
+##
+#print(IMC)
+
+##
+#Metodo pt_BR
+##
+print(f"O resultado do IMC é : {IMC:.2f}")
