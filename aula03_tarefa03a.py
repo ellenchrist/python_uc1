ee7af2d89b7bf821d94f7ff54c034f620205fb04
ellenchrist <ellenchrist85@hotmail.com>
2025-03-07 21:45:08 -0300

    Aula03_tarefa03a

---
+++ aula03_tarefa03a.py
@@ -0,0 +1,31 @@
+x=10
+print(f"O valor inicial de x é {x}")
+
+resultado=x
+
+resultado+=5
+print(f"O novo valor da soma é {resultado}")
+
+resultado=x
+resultado-=3
+print(f"O novo valor da subtracao é {resultado}")
+
+resultado=x
+resultado*=2
+print(f"O novo valor da multiplicação é {resultado}")
+
+resultado=x
+resultado/=4
+print(f"O novo valor da divisão é {resultado}")
+
+resultado=x
+resultado//=3
+print(f"O novo valor da divisão inteira é {resultado}")
+
+resultado=x
+resultado%=2
+print(f"O novo valor com módulo é {resultado}")
+
+resultado=x
+resultado**=3
+print(f"O novo valor da exponenciação é {resultado}")
